28da8c09dea102b644757ed4165b1544f1f12199
Максим Рыжков <mirouse444@gmail.com>
2026-08-31 02:28:20 +0300

    Fix Borders, Add Weapon

--- Assets/Weapon/WeaponSkripts/WeaponSwitcher.cs
+++ Assets/Weapon/WeaponSkripts/WeaponSwitcher.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponSwitcher : MonoBehaviour
 {
@@ -6,22 +7,31 @@ public class WeaponSwitcher : MonoBehaviour
     [SerializeField] private PlayerInputController _input;
     [SerializeField] private Inventory _inventory;
 
+    [SerializeField] private Button[]  _buttons = new Button[4]; 
     private GameObject _currentActiveWeapon;
     
     private void OnEnable()
     {
         _input.OnButton1 += Button1Pressed;
+        _buttons[0].onClick.AddListener(Button1Pressed);
         _input.OnButton2 += Button2Pressed;
+        _buttons[1].onClick.AddListener(Button2Pressed);
         _input.OnButton3 += Button3Pressed;
+        _buttons[2].onClick.AddListener(Button3Pressed);
         _input.OnButton4 += Button4Pressed;
+        _buttons[3].onClick.AddListener(Button4Pressed);
     }
 
     private void OnDisable()
     {
         _input.OnButton1 -= Button1Pressed;
+        _buttons[0].onClick.RemoveListener(Button1Pressed);
         _input.OnButton2 -= Button2Pressed;
+        _buttons[1].onClick.RemoveListener(Button2Pressed);
         _input.OnButton3 -= Button3Pressed;
+        _buttons[2].onClick.RemoveListener(Button3Pressed);
         _input.OnButton4 -= Button4Pressed;
+        _buttons[3].onClick.RemoveListener(Button4Pressed);
     }
 
     private void Button1Pressed() => SelectCategory(WeaponCategory.AssaultRifle);
